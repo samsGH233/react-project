09a2029b7768b6ddd33d37cd0f9739820adcc537
samsGH233 <S.fatogun@wlv.ac.uk>
2025-05-28 14:51:41 +0100

    Add files via upload

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,12 +1,35 @@
-
+import React, { useEffect, useState } from 'react';
 import logo from '../images/connectinSkillz logo.avif';
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose } from 'react-icons/ai';
 
 const NavBar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    useEffect(() => {
+        if (isOpen) {
+            document.body.classList.add('no-scroll');
+            document.documentElement.classList.add('no-scroll');
+        } else {
+            document.body.classList.remove('no-scroll');
+            document.documentElement.classList.remove('no-scroll');
+        }
+    }, [isOpen]);
+
 
     return (
         <nav className="navbar">
+
             <img className="logo" src={logo} alt="My description" width="50" />
-            <ul className="nav-links">
+
+            <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+                <GiHamburgerMenu />
+            </div>
+
+            <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
+                <li className="close-icon">
+                    <AiOutlineClose onClick={() => setIsOpen(false)} />
+                </li>
                 <li><a href="#home">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#services">Courses</a></li>
